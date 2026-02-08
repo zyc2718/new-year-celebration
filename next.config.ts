3ee5b1faa7ab4d5e378cea026795e33fe95dcc1b
yingjichangkong-code <19836062191@163.com>
2026-02-09 01:08:37 +0800

    Standardize build and deployment for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   output: 'export',
-  basePath: '/new-year-celebration',
   images: {
     unoptimized: true,
   },
